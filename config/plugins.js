3df6037dd28b3c9823070877aaa34c2a30c4ac38
Ujith Nimantha <ujithnimantha@gmail.com>
2024-01-23 10:05:02 +0300

    Added a comment in plugins.js file

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,18 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   upload: {
     config: {
-      provider: "aws-s3",
+      provider: 'aws-s3',
       providerOptions: {
-        baseUrl: env("CDN_URL"),
-        rootPath: env("CDN_ROOT_PATH"),
+        baseUrl: env('CDN_URL'),
+        rootPath: env('CDN_ROOT_PATH'),
         s3Options: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
           params: {
-            ACL: env("AWS_ACL", "public-read"),
-            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-            Bucket: env("AWS_BUCKET"),
+            ACL: env('AWS_ACL', 'public-read'),
+            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+            Bucket: env('AWS_BUCKET'),
           },
         },
       },
@@ -24,3 +24,5 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+// End
