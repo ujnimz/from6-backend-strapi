4993b60e00c4ad9f87d7dd793fe1f2743f9c35f7
Ujith Nimantha <ujithnimantha@gmail.com>
2024-01-24 23:18:46 +0300

    Media directory changed to from6-media from root in CloudFlare bucket

--- config/plugins.js
+++ config/plugins.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
     config: {
       provider: "strapi-provider-cloudflare-r2",
       providerOptions: {
+        rootPath: env("CF_BUCKET_DIRECTORY"),
         accessKeyId: env("CF_ACCESS_KEY_ID"),
         secretAccessKey: env("CF_ACCESS_SECRET"),
         /**
@@ -30,29 +31,3 @@ module.exports = ({ env }) => ({
   },
   // ...
 });
-
-// ~/strapi-aws-s3/backend/config/plugins.js
-
-// module.exports = ({ env }) => ({
-//   upload: {
-//     config: {
-//       provider: "aws-s3",
-//       providerOptions: {
-//         accessKeyId: env("AWS_ACCESS_KEY_ID"),
-//         secretAccessKey: env("AWS_ACCESS_SECRET"),
-//         region: env("AWS_REGION"),
-//         rootPath: "images",
-//         params: {
-//           ACL: env("AWS_ACL", "public-read"),
-//           signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-//           Bucket: env("AWS_BUCKET"),
-//         },
-//       },
-//       actionOptions: {
-//         upload: {},
-//         uploadStream: {},
-//         delete: {},
-//       },
-//     },
-//   },
-// });
