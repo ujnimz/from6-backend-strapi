2ed7ce2278f224045c77c75b254e00eaba5e6ab0
Ujith Nimantha <ujithnimantha@gmail.com>
2024-01-25 16:52:28 +0300

    Fixed conflict

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,32 +1,32 @@
-module.exports = ({ env }) => [
-  "strapi::logger",
-  "strapi::errors",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+module.exports = ({env}) => [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
   /* Beginning of snippet */
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
             "'self'",
-            "data:",
-            "blob:",
-            env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, ""),
+            'data:',
+            'blob:',
+            env('CF_PUBLIC_ACCESS_URL').replace(/^https?:\/\//, ''),
           ],
-          "media-src": [
+          'media-src': [
             "'self'",
-            "data:",
-            "blob:",
-            env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, ""),
+            'data:',
+            'blob:',
+            env('CF_PUBLIC_ACCESS_URL').replace(/^https?:\/\//, ''),
           ],
           upgradeInsecureRequests: null,
         },
